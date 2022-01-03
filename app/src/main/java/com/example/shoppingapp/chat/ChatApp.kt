5db50d4010ec0601b45dd3ed0b8f879922a8c880
Oscar Ho <oscar.ho0210@gmail.com>
2022-01-04 00:33:33 +0800

    Add firebase database(1/4)

--- app/src/main/java/com/example/shoppingapp/chat/ChatApp.kt
+++ app/src/main/java/com/example/shoppingapp/chat/ChatApp.kt
@@ -2,11 +2,20 @@ package com.example.shoppingapp.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppingapp.R
 
 class ChatApp : AppCompatActivity() {
+
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_app)
+
+        userList = ArrayList()
+        adapter = UserAdapter(this,userList)
     }
 }
