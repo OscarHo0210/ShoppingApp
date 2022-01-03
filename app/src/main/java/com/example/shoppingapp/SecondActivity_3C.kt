4c6740ce3c3ab3f1443c9e1bfcc1e539133be693
jpan <j.pan.0923551124@gmail.com>
2022-01-03 22:45:34 +0800

    3C_first commit

---
+++ app/src/main/java/com/example/shoppingapp/SecondActivity_3C.kt
@@ -0,0 +1,18 @@
+package com.example.shoppingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SecondActivity_3C : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_3c)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            startActivity(Intent(this,ThirdActivity_3C_one::class.java))
+        }
+    }
+}
